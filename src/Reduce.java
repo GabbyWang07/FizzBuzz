906026b9ec998eb06716d064a2aa54f9a6e1a089
Zihan Wang <zihan@Zihans-MacBook-Pro.local>
2023-09-14 09:08:56 -0400

    Reduce complete

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int k = 0;
+        int i = 100;
+        while (i > 0){
+            if (i % 2 == 0){
+
+                i = i / 2;
+
+            } else {
+
+                i = i - 1;
+
+            }
+            k++;
+        }
+        System.out.println(k);
+    }
 }
