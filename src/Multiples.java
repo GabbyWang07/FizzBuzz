9ff6d048ad00cb6754a95e2fd1a8a5eb92aff129
Zihan Wang <zihan@Zihans-MacBook-Pro.local>
2023-09-14 13:00:07 -0400

    Multiples r2 complete

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,13 +1,39 @@
 public class Multiples {
     public static void main(String[] args) {
+
         int k = 0;
         for (int i = 1; i < 100; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
 
                 k++;
             }
+        }
+
     }
-        System.out.println(k);
+
+    public int main(int n, int a, int b) {
+        int k = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+
+                k++;
+
+            }
+    }
+        return k;
+    }
+
+    public int main() {
+        int k = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+
+                k++;
+
+            }
+        }
+        return k;
     }
 }
+
 
